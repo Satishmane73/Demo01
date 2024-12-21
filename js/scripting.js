ba12c92d4942a2dc33d40d25b7a3dccbae95dc7d
Satishmane73 <satishmanevalid@gmail.com>
2024-12-21 19:23:04 +0530

    Code Deployed

---
+++ js/scripting.js
@@ -0,0 +1,5 @@
+document.getElementById('button').addEventListener('click', function () {
+    const blessMessage = document.getElemementById('blessMesssage');
+    blessMessage.innnerText = "May Lord Ganesha bless you with wisdom and success !";
+    blessMessage.style.display = 'block';
+});
